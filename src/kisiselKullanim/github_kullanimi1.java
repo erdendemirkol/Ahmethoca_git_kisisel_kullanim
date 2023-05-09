48db3a3a2635c59199fbf1fc4b709d21a3839725
erdendemirkol <123850988+erdendemirkol@users.noreply.github.com>
2023-05-09 10:24:16 -0700

    Revert "helloWorld added"
    This reverts commit 67424d2a63b5159fe7efb5b5e9aaee0061c01566.

--- src/kisiselKullanim/github_kullanimi1.java
+++ src/kisiselKullanim/github_kullanimi1.java
@@ -3,8 +3,6 @@ package kisiselKullanim;
 public class github_kullanimi1 {
 
     public static void main(String[] args) {
-
-        System.out.println("That's it");
         System.out.println("Hello World");
     }
 
