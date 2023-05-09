67424d2a63b5159fe7efb5b5e9aaee0061c01566
erdendemirkol <123850988+erdendemirkol@users.noreply.github.com>
2023-05-09 10:23:57 -0700

    helloWorld added

--- src/kisiselKullanim/github_kullanimi1.java
+++ src/kisiselKullanim/github_kullanimi1.java
@@ -3,6 +3,8 @@ package kisiselKullanim;
 public class github_kullanimi1 {
 
     public static void main(String[] args) {
+
+        System.out.println("That's it");
         System.out.println("Hello World");
     }
 
